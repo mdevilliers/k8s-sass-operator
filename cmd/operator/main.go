db404cff0b4ff25848f8bbceb825e8be96032dd0
Mark deVilliers <markdevilliers@gmail.com>
2016-11-04 16:31:46 +0000

    minimal operator

---
+++ cmd/operator/main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/url"
+	"os"
+	"time"
+
+	"github.com/Sirupsen/logrus"
+
+	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/client/leaderelection"
+	"k8s.io/kubernetes/pkg/client/record"
+	"k8s.io/kubernetes/pkg/client/restclient"
+	"k8s.io/kubernetes/pkg/client/unversioned"
+)
+
+var (
+	masterHost  string
+	tlsInsecure bool
+	certFile    string
+	keyFile     string
+	caFile      string
+	namespace   string
+)
+
+var (
+	leaseDuration = 15 * time.Second
+	renewDuration = 5 * time.Second
+	retryPeriod   = 3 * time.Second
+)
+
+func init() {
+
+	flag.StringVar(&masterHost, "master", "", "API Server addr, e.g. ' - NOT RECOMMENDED FOR PRODUCTION - http://127.0.0.1:8080'. Omit parameter to run in on-cluster mode and utilize the service account token.")
+	flag.StringVar(&certFile, "cert-file", "", " - NOT RECOMMENDED FOR PRODUCTION - Path to public TLS certificate file.")
+	flag.StringVar(&keyFile, "key-file", "", "- NOT RECOMMENDED FOR PRODUCTION - Path to private TLS certificate file.")
+	flag.StringVar(&caFile, "ca-file", "", "- NOT RECOMMENDED FOR PRODUCTION - Path to TLS CA file.")
+
+	namespace = os.Getenv("NAMESPACE")
+	if len(namespace) == 0 {
+		namespace = "default"
+	}
+}
+
+func main() {
+
+	id, err := os.Hostname()
+	if err != nil {
+		logrus.Fatalf("failed to get hostname: %v", err)
+	}
+
+	leaderelection.RunOrDie(leaderelection.LeaderElectionConfig{
+		EndpointsMeta: api.ObjectMeta{
+			Namespace: namespace,
+			Name:      "sass-operator",
+		},
+		Client: MustCreateClient(masterHost, tlsInsecure, &restclient.TLSClientConfig{
+			CertFile: certFile,
+			KeyFile:  keyFile,
+			CAFile:   caFile,
+		}),
+		EventRecorder: &record.FakeRecorder{},
+		Identity:      id,
+		LeaseDuration: leaseDuration,
+		RenewDeadline: renewDuration,
+		RetryPeriod:   retryPeriod,
+		Callbacks: leaderelection.LeaderCallbacks{
+			OnStartedLeading: func(stop <-chan struct{}) {
+				logrus.Info("ego leader...")
+				<-stop
+				logrus.Info("no longer the leader.")
+			},
+			OnStoppedLeading: func() {
+				logrus.Fatalf("leader election lost.")
+			},
+			OnNewLeader: func(identity string) {
+				logrus.Info("new leader :", identity)
+
+			},
+		},
+	})
+}
+
+// tlsConfig isn't modified inside this function.
+// The reason it's a pointer is that it's not necessary to have tlsconfig to create a client.
+func MustCreateClient(host string, tlsInsecure bool, tlsConfig *restclient.TLSClientConfig) *unversioned.Client {
+	if len(host) == 0 {
+		c, err := unversioned.NewInCluster()
+		if err != nil {
+			panic(err)
+		}
+		return c
+	}
+	cfg := &restclient.Config{
+		Host:  host,
+		QPS:   100,
+		Burst: 100,
+	}
+	hostUrl, err := url.Parse(host)
+	if err != nil {
+		panic(fmt.Sprintf("error parsing host url %s : %v", host, err))
+	}
+	if hostUrl.Scheme == "https" {
+		cfg.TLSClientConfig = *tlsConfig
+		cfg.Insecure = tlsInsecure
+	}
+	c, err := unversioned.New(cfg)
+	if err != nil {
+		panic(err)
+	}
+	return c
+}
