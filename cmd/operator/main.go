d76c24508ee59d45383151c6788f533d5544c1a1
Mark deVilliers <markdevilliers@gmail.com>
2016-11-04 17:40:11 +0000

    we don't need no stinking warnings

--- cmd/operator/main.go
+++ cmd/operator/main.go
@@ -33,10 +33,10 @@ var (
 
 func init() {
 
-	flag.StringVar(&masterHost, "master", "", "API Server addr, e.g. ' - NOT RECOMMENDED FOR PRODUCTION - http://127.0.0.1:8080'. Omit parameter to run in on-cluster mode and utilize the service account token.")
-	flag.StringVar(&certFile, "cert-file", "", " - NOT RECOMMENDED FOR PRODUCTION - Path to public TLS certificate file.")
-	flag.StringVar(&keyFile, "key-file", "", "- NOT RECOMMENDED FOR PRODUCTION - Path to private TLS certificate file.")
-	flag.StringVar(&caFile, "ca-file", "", "- NOT RECOMMENDED FOR PRODUCTION - Path to TLS CA file.")
+	flag.StringVar(&masterHost, "master", "", "API Server addr, e.g. 'http://127.0.0.1:8080'. Omit parameter to run in on-cluster mode and utilize the service account token.")
+	flag.StringVar(&certFile, "cert-file", "", "Path to public TLS certificate file.")
+	flag.StringVar(&keyFile, "key-file", "", "Path to private TLS certificate file.")
+	flag.StringVar(&caFile, "ca-file", "", "Path to TLS CA file.")
 
 	namespace = os.Getenv("NAMESPACE")
 	if len(namespace) == 0 {
@@ -46,10 +46,14 @@ func init() {
 
 func main() {
 
-	id, err := os.Hostname()
+	hostname, err := os.Hostname()
 	if err != nil {
 		logrus.Fatalf("failed to get hostname: %v", err)
 	}
+
+	logrus.Info("sass-operator starting...")
+	logrus.Info("hostname", hostname)
+	logrus.Info("namespace", namespace)
 
 	leaderelection.RunOrDie(leaderelection.LeaderElectionConfig{
 		EndpointsMeta: api.ObjectMeta{
@@ -83,6 +87,7 @@ func main() {
 	})
 }
 
+// TODO : pick this apart
 // tlsConfig isn't modified inside this function.
 // The reason it's a pointer is that it's not necessary to have tlsconfig to create a client.
 func MustCreateClient(host string, tlsInsecure bool, tlsConfig *restclient.TLSClientConfig) *unversioned.Client {
